994addd28844d7202c9a32853a1dd2aa9238a966
ahaynie1 <allisonhaynie7@gmail.com>
2023-10-13 22:37:02 -0700

    work on circletype

--- scripts.js
+++ scripts.js
@@ -10,4 +10,4 @@ document.addEventListener("DOMContentLoaded", function () {
     mainContent.scrollIntoView({ behavior: "smooth" });
   });
 });
-new CircleType(document.getElementById("circletype")).radius(5);
+new CircleType(document.getElementById("circletype")).radius(10);
