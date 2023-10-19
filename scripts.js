886128ae379a264faef4bd1621901ecc43603484
ahaynie1 <allisonhaynie7@gmail.com>
2023-10-19 11:24:58 -0700

    remove circletype radius

--- scripts.js
+++ scripts.js
@@ -10,4 +10,4 @@ document.addEventListener("DOMContentLoaded", function () {
     mainContent.scrollIntoView({ behavior: "smooth" });
   });
 });
-new CircleType(document.getElementById("circletype")).radius(10);
+new CircleType(document.getElementById("circletype"));
