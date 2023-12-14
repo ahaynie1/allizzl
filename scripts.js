9729698617439787dcdf07994f2ea79a7a6f5069
ahaynie1 <allisonhaynie7@gmail.com>
2023-12-13 23:02:45 -0800

    Add Gallery Arrow Functionality

--- scripts.js
+++ scripts.js
@@ -32,3 +32,69 @@ document.addEventListener("DOMContentLoaded", function () {
     event: "resize",
   });
 });
+
+// BACK AND FORTH GALLERY BUTTONS
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to navigate to the next item
+  function navigateToNextItem() {
+    // Get the current item's position from the URL
+    var currentPosition = getCurrentItemPosition();
+
+    // Find the next item's position
+    var nextPosition = currentPosition + 1;
+
+    // Navigate to the next item
+    window.location.href = findItemUrlByPosition(nextPosition);
+  }
+
+  // Function to navigate to the previous item
+  function navigateToPrevItem() {
+    // Get the current item's position from the URL
+    var currentPosition = getCurrentItemPosition();
+
+    // Find the previous item's position
+    var prevPosition = currentPosition - 1;
+
+    // Navigate to the previous item
+    window.location.href = findItemUrlByPosition(prevPosition);
+  }
+
+  // Add event listeners to arrow buttons
+  document
+    .getElementById("nextButton")
+    .addEventListener("click", navigateToNextItem);
+  document
+    .getElementById("prevButton")
+    .addEventListener("click", navigateToPrevItem);
+});
+
+// Function to get the current item's position from the HTML
+function getCurrentItemPosition() {
+  // Get the current item's element
+  var currentItemElement = document.querySelector(".gallery-item.current");
+
+  // Check if the element is found
+  if (currentItemElement) {
+    // Extract the position from the data-position attribute
+    var currentPosition = parseInt(currentItemElement.dataset.position);
+
+    // Return the position
+    return currentPosition;
+  }
+
+  // Return a default value or handle the case when the element is not found
+  return 1; // Default position (adjust as needed)
+}
+
+// Function to find the item URL based on its position
+function findItemUrlByPosition(position) {
+  var items = document.querySelectorAll(".gallery-item");
+  for (var i = 0; i < items.length; i++) {
+    if (parseInt(items[i].dataset.position) === position) {
+      return items[i].href;
+    }
+  }
+  // Handle edge cases, e.g., when the last item is reached
+  return null;
+}
