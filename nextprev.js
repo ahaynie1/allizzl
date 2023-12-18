8d265be04484f7e8f1fda35bb9336a777d990918
ahaynie1 <allisonhaynie7@gmail.com>
2023-12-18 01:29:26 -0800

    Update nextprev.js
    forgo local storage

--- nextprev.js
+++ nextprev.js
@@ -13,18 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
     return galleryInfo;
   }
 
-  // Function to store the gallery information in local storage
-  function storeGalleryInfo() {
-    var galleryInfo = getGalleryInfo();
-
-    try {
-      localStorage.setItem("galleryInfo", JSON.stringify(galleryInfo));
-      console.log("Gallery Info Stored:", galleryInfo);
-    } catch (error) {
-      console.error("Error storing gallery info:", error);
-    }
-  }
-
   // Function to navigate to the next item
   function navigateToNextItem() {
     var currentPosition = getCurrentItemPosition();
@@ -47,39 +35,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Function to get the current item's position from the stored data
   function getCurrentItemPosition() {
-    var storedGalleryInfo = JSON.parse(localStorage.getItem("galleryInfo"));
+    var galleryInfo = getGalleryInfo();
+    var currentItemHref = window.location.href;
+    var currentItem = galleryInfo.find((item) => item.href === currentItemHref);
 
-    if (storedGalleryInfo) {
-      var currentItemHref = window.location.href;
-      var currentItem = storedGalleryInfo.find(
-        (item) => item.href === currentItemHref
-      );
-
-      if (currentItem) {
-        return currentItem.position;
-      }
+    if (currentItem) {
+      return currentItem.position;
     }
 
-    console.log("Current Position not found. Defaulting to 1.");
-    return 1;
+    console.log("Current Position not found. Defaulting to 0.");
+    return 0;
   }
 
   // Function to find the item URL based on its position
   function findItemUrlByPosition(position) {
-    var storedGalleryInfo = JSON.parse(localStorage.getItem("galleryInfo"));
+    var galleryInfo = getGalleryInfo();
+    var item = galleryInfo.find((item) => item.position === position);
 
-    if (storedGalleryInfo) {
-      var item = storedGalleryInfo.find((item) => item.position === position);
-      if (item) {
-        return item.href;
-      }
+    if (item) {
+      return item.href;
     }
 
     return null;
   }
-
-  // Call the storeGalleryInfo function to store the information when the page loads
-  storeGalleryInfo();
 
   // Add event listeners to arrow buttons
   document
