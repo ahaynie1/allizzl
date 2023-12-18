01fef1dcae8de9e98000e8e357df1e5274ccc238
ahaynie1 <allisonhaynie7@gmail.com>
2023-12-18 01:31:59 -0800

    Update nextprev.js

--- nextprev.js
+++ nextprev.js
@@ -16,8 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // Function to navigate to the next item
   function navigateToNextItem() {
     var currentPosition = getCurrentItemPosition();
+    console.log("Current Position:", currentPosition);
+
     var nextPosition = currentPosition + 1;
+    console.log("Next Position:", nextPosition);
+
     var nextUrl = findItemUrlByPosition(nextPosition);
+    console.log("Next URL:", nextUrl);
 
     if (nextUrl) {
       window.location.href = nextUrl;
@@ -29,15 +34,29 @@ document.addEventListener("DOMContentLoaded", function () {
   // Function to navigate to the previous item
   function navigateToPrevItem() {
     var currentPosition = getCurrentItemPosition();
+    console.log("Current Position:", currentPosition);
+
     var prevPosition = currentPosition - 1;
-    window.location.href = findItemUrlByPosition(prevPosition);
+    var prevUrl = findItemUrlByPosition(prevPosition);
+    console.log("Previous URL:", prevUrl);
+
+    if (prevUrl) {
+      window.location.href = prevUrl;
+    } else {
+      console.error("Invalid URL for previous item");
+    }
   }
 
   // Function to get the current item's position from the stored data
   function getCurrentItemPosition() {
     var galleryInfo = getGalleryInfo();
+    console.log("Gallery Info:", galleryInfo);
+
     var currentItemHref = window.location.href;
+    console.log("Current Item Href:", currentItemHref);
+
     var currentItem = galleryInfo.find((item) => item.href === currentItemHref);
+    console.log("Current Item:", currentItem);
 
     if (currentItem) {
       return currentItem.position;
@@ -50,12 +69,16 @@ document.addEventListener("DOMContentLoaded", function () {
   // Function to find the item URL based on its position
   function findItemUrlByPosition(position) {
     var galleryInfo = getGalleryInfo();
+    console.log("Searching for position:", position);
+
     var item = galleryInfo.find((item) => item.position === position);
+    console.log("Item:", item);
 
     if (item) {
       return item.href;
     }
 
+    console.error("URL not found for position:", position);
     return null;
   }
 
